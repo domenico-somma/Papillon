653fd81dd224d621903fe36e47aebd76035479d7
Domenico <34346930+domenico-somma@users.noreply.github.com>
2018-01-03 10:40:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='papillon',
-    version='0.0.7',
+    version='0.0.8',
     py_modules=['papillon'],
     description='A Python module to read and plot Galaxy RNA-seq data',
     author='Domenico Somma',
